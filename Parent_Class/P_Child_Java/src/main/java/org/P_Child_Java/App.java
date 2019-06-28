5691e1abc8768fd100831d2a0a66f5cf85cb47e6
LaxmiBaragukar <laxmimb235@gmail.com>
2019-06-28 10:34:29 +0530

    first commit

---
+++ Parent_Class/P_Child_Java/src/main/java/org/P_Child_Java/App.java
@@ -0,0 +1,25 @@
+package org.P_Child_Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App implements Interface
+{
+	 
+	 String x;
+	
+	public String getX() 
+	{
+		return x;
+	}
+	public void setX(String x)
+	{
+		this.x = x;
+	}
+	
+	}
+	  
+	
+	
+	 
---
+++ Parent_Class/P_Child_Java/src/main/java/org/P_Child_Java/Interface.java
@@ -0,0 +1,9 @@
+package org.P_Child_Java;
+
+public interface Interface 
+{
+	public String getX();
+	public void setX(String x);
+	
+
+}
